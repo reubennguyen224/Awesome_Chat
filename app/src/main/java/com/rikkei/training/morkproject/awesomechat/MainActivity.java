b54065228ca92ca5d0d76adde50ceb942215327e
reuben <markharrisons1235@gmail.com>
2022-07-20 09:22:39 +0700

    Initial commit

---
+++ app/src/main/java/com/rikkei/training/morkproject/awesomechat/MainActivity.java
@@ -0,0 +1,24 @@
+package com.rikkei.training.morkproject.awesomechat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) getSupportActionBar().hide();
+
+        final Handler handler = new Handler();
+        handler.postDelayed(() -> {
+            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            finish();
+        }, 3000);
+    }
+}
