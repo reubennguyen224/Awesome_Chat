05c3c2e5fcfc34fa2413fc8aac3e8c9d7689b817
reubennguyen224 <markharrisons1235@gmail.com>
2022-07-27 14:24:17 +0700

    update

---
+++ app/src/main/java/com/rikkei/training/morkproject/awesomechat/OnUserClickedListener.java
@@ -0,0 +1,7 @@
+package com.rikkei.training.morkproject.awesomechat;
+
+import com.rikkei.training.morkproject.awesomechat.Model.User;
+
+public interface OnUserClickedListener {
+    void onUserLongClicked(User user, int position);
+}
