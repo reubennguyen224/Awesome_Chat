d44bd350402ab9e05e5ad42285be17fbe27285c2
reubennguyen224 <markharrisons1235@gmail.com>
2022-07-27 13:57:05 +0700

    update

--- app/src/main/java/com/rikkei/training/morkproject/awesomechat/Fragments/HomeFragment.java
+++ app/src/main/java/com/rikkei/training/morkproject/awesomechat/Fragments/HomeFragment.java
@@ -17,6 +17,7 @@ import android.widget.TextView;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
